802e5b6fe736418b25472217d585e57b21611f21
caiofariaas <caiofarias077@gmail.com>
2024-02-20 11:08:11 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -19,14 +17,13 @@ public class Main {
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDesc("Desc Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1 + "\n");
-        System.out.println(curso2 + "\n");
-        System.out.println(mentoria + "\n");
+//        System.out.println(curso1 + "\n");
+//        System.out.println(curso2 + "\n");
+//        System.out.println(mentoria + "\n");
 
 // Polimorfismo
 //  Tudo que existe em Conteudo existe em Curso
@@ -35,6 +32,36 @@ public class Main {
 //        Conteudo conteudo = new Curso();
 
 
+//  Criando um bootcamp e adicionando curoso a ele
 
+        Bootcamp bootcamp1 = new Bootcamp();
+
+        bootcamp1.setNome("Bootcamp Java Dev");
+        bootcamp1.setDesc("Desc Bootcamp Java dev");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria);
+
+//  Criando Dev's
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Camila");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("joão");
+
+        dev1.inscreverBootcamp(bootcamp1);
+        dev2.inscreverBootcamp(bootcamp1);
+
+
+        dev1.progredir();
+        System.out.println(dev1.getConteudosInscritos());
+        System.out.println(dev1.getConteudosConcluidos());
+
+        System.out.println();
+
+        dev2.progredir();
+        System.out.println(dev2.getConteudosInscritos());
+        System.out.println(dev2.getConteudosConcluidos());
     }
 }
